7e045fa5337197f7b95a3726bd9bf4b88adc4d7c
orihara <oriri75@gmail.com>
2018-10-08 17:17:42 +0900

    create_login

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,21 @@
 class BlogsController < ApplicationController
+  before_action :require_login,only:[:new,:edit,:show,:destroy]
   before_action :set_blog,only:[:show,:edit,:update,:destroy]
- 
+
+  def current_user
+    @current_user ||= User.find_by(id:session[:user_id])
+  end
+
+
   def index
     @blogs = Blog.all
   end
-  
+
   def create
     Blog.create(blog_params)
     redirect_to new_blog_path
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blog_params)
@@ -17,10 +23,10 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def show
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -29,10 +35,10 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path,notice:"ブログを編集しました！"
@@ -40,24 +46,31 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
   @blog.destroy
   redirect_to blogs_path,notice:"ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
   private
-  
+
   def blog_params
     params.require(:blog).permit(:title,:content)
   end
-  
+
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "ログインが必要です"
+    redirect_to new_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ get '/tops', to: 'tops#index'
   resources :blogs do
     collection do
       post :confirm
+      resources :users, only: [:new,:create,:show]
+      resources :sessions, only:[:new, :create, :destroy]
+
     end
   end
 end
